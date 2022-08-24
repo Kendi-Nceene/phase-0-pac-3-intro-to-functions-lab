9796459f3d9738d1a6aca1459361263931382966
+ <+>
2022-08-24 09:45:20 +0300

    Task done

---
+++ index.js
@@ -0,0 +1,39 @@
+//shout (string)
+function shout(string) {
+    return string.toUpperCase();
+  }
+  "Hello!".toUpperCase(); // 'HELLO!'
+  
+//whisper(string)
+function whisper(string) {
+    return string.toLowerCase();
+  }
+  "hello!".toLowerCase(); // 'hello!'
+
+// logShout(string)
+  function  logShout(string){
+    console.log("HELLO"); 
+  } //"HELLO"
+
+//logWhisper(string)
+ function logWhisper(string){
+    console.log("hello");
+ } //"hello"
+
+ //sayHiToHeadphonedRoommate(string)
+ 
+ const string = "Hello!";
+
+console.log(string);
+console.log(string.toUpperCase());
+string === string.toUpperCase();
+
+function sayHiToHeadphonedRoommate(string){
+    return string.toLowerCase();
+}
+"I can'\t hear you!".toLowerCase();
+"YES INDEED!".toUpperCase();
+console.log("I would love to!");
+
+
+
